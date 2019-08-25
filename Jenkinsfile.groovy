09c4014698db52a9684f19342d3cf0095ce90579
jsartbaeva <jsartbaeva91@gmail.com>
2019-08-25 17:26:15 -0500

    added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -70,7 +70,7 @@ pipeline{
             echo "Done"
         }
         failure {
-            mail to:  "jsartbaeva90@gmail.com", subject: "job", body: "job completed"
+            mail to:  "farrukhsadykov@gmail.com", subject: "job", body: "job completed"
         }
     }
 }
